7e2c0c8c2b5507a7d9ce740f47c4d6333d792560
Andrew Valerikov <andreivalerikov1103@gmail.com>
2023-12-18 14:07:11 +0300

    Add libraries

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk, filedialog
+from docx import Document
+from docx.shared import Pt, Cm, Inches
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import fitz
+import language_tool_python
