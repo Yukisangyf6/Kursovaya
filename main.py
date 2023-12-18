98e44257a428d0389a1bd70ffcbbbc363cd6c6d9
Andrew Valerikov <andreivalerikov1103@gmail.com>
2023-12-18 14:10:32 +0300

    Add buttons with style and center them

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 import fitz
 import language_tool_python
 class DocumentFormatterApp:
+    # Инициализация приложения
+    def __init__(self, master):
+        # Установка главного окна
+        self.master = master
+        master.title("Форматирование документа")
+
+        # Определение стилей для кнопок
+        style = ttk.Style()
+        style.theme_use('clam')
+        style.configure('Text.TButton', background='#568fba', padding=2, font=('Google Sans', 14), foreground='white')
+        style.map('Text.TButton', background=[('active', '#274873')])
+
+        # Центрирование элементов в окне
+        master.columnconfigure(0, weight=1)
+        master.columnconfigure(1, weight=1)
+        master.columnconfigure(2, weight=1)
